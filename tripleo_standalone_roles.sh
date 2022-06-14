dbcecc645743155e9c46a21751c844ce9f49ca78
Bogdan Dobrelya <bogdando@yahoo.com>
2022-06-14 16:46:27 +0200

    Get more context about hiera data and propose defaults
    Also allow to go without prefixes
    
    Signed-off-by: Bogdan Dobrelya <bogdando@yahoo.com>

--- tripleo_standalone_roles.sh
+++ tripleo_standalone_roles.sh
@@ -18,6 +18,9 @@ PUPPET=/opt/Projects/gitrepos/OOO/tripleo-heat-templates/deployment/nova/nova-ba
 MATCH="nova_|nova_libvirt_|nova_compute_"
 VARS=/opt/Projects/gitrepos/OOO/tripleo-ansible/tripleo_ansible/roles/tripleo_$SVC/defaults/main.yml
 
+# Role vars will be/expected to be prefixed with that:
+PREFIX="tripleo_${SVC}_"
+
 IGNORE="
 service_net_map
 service_data
@@ -39,77 +42,107 @@ DEFAULT
 "
 
 if ! git diff --quiet ; then 
-  echo "Stage or commit work tree changes before running it!"
+  echo "ERROR: Stage or commit work tree changes before running it!"
   exit 1
 fi
 
 # filter out multi-world acronyms like TLSCA then normalize acronyms as camelCase
 filter="sed -r 's/TLS/Tls/g;s/CA/Ca/g;s/([A-Z])([A-Z]*)([A-Z][a-z])/\1\L\2\u\3/g'"
 yq -r '.parameters|keys[]' $THT | eval $filter |sort -h | tee /tmp/$SVC | \
-  python -c "from pprint import pprint; import fileinput; import re; pprint([re.sub('([a-z0-9])([A-Z])', r'\1_\2', re.sub('(.)([A-Z][a-z]+)', r'\1_\2', str)).lower().strip() for str in fileinput.input()])" | \
+  python -c "import fileinput; import re; print([str.strip() + ' ' + re.sub('([a-z0-9])([A-Z])', r'\1_\2', re.sub('(.)([A-Z][a-z]+)', r'\1_\2', str)).lower().strip() for str in fileinput.input()])" | \
   yq -r '.[]' >  /tmp/${SVC}_snake
 
 # prepare group vars to wire-in for tht to call the role
 yq -r '.parameters|keys[]' $THT | eval $filter |sort -h | tee /tmp/$SVC |\
-  python -c "from pprint import pprint; import fileinput; import re; print([re.sub('([a-z0-9])([A-Z])', r'\1_\2', re.sub('(.)([A-Z][a-z]+)', r'\1_\2', str)).lower().strip() + ': {get_param: ' + str.strip() + '}' for str in fileinput.input()])" | \
+  python -c "import fileinput; import re; print([re.sub('([a-z0-9])([A-Z])', r'\1_\2', re.sub('(.)([A-Z][a-z]+)', r'\1_\2', str)).lower().strip() + ': {get_param: ' + str.strip() + '}' for str in fileinput.input()])" | \
   yq -r '.[]' >  /tmp/${SVC}_group_vars_wire_in
 
-# prepare ansible config vars based on puppet base hiera data in tht
-namespace=".outputs.role_data.value.config_settings,.resources.RoleParametersValue.properties.value"
-yq -r "$namespace" $THT | awk  '/::/ {print}' > /tmp/${SVC}_config_defaults
-yq -r "$namespace" $THT | \
+# Prepare ansible config vars based on puppet base hiera data in tht
+
+# where to look for hiera values (both in $THT and $PUPPET files)
+hieraloc='.outputs.role_data.value.config_settings,.resources.RoleParametersValue.properties.value'
+# enter topscope funcs having the same output data view (like with and w/o map_merge)
+enterheatfuncs='.,.map_replace?,.map_merge?,.get_attr?,.str_replace?,.list_concat? | select(.!=null)[] | to_entries[] | select(.key|type!="number")'
+# is (not/) a heat func
+notheatfunc='select(.key|test("^(if$|map_|list_|get_|str_)")|not)'
+isheatfunc='select(.key|test("^(if$|map_|list_|get_|str_)"))'
+# is (not/) an object
+notobj='select (.value|type!="object")'
+isobj='select (.value|type=="object")'
+
+# filter hiera data keys with direct values and directly defined defaults
+# to filter it later (to suggest default values for role vars matching hiera keys)
+yq -r "$hieraloc | $enterheatfuncs | $notheatfunc | $notobj" \
+  $PUPPET $THT > /tmp/${SVC}_config_defaults
+
+# get direct t-h-t params substitutions in hiera values
+# to filter it later (to drop role vars for hiera keys that already match other defined role vars)
+yq -r "$hieraloc | $enterheatfuncs | $isobj | select(.value.get_param!=null) | select(.value.get_param|type==\"string\")" \
+  $PUPPET $THT > /tmp/${SVC}_config_substitutions
+
+# get complex hiera data (conditions/templating) that requires special handling:
+# cannot assume a default, nor can assume if it misses a role var or not
+yq -r "$hieraloc | $enterheatfuncs | $isobj " \
+  $PUPPET $THT > /tmp/${SVC}_config_special_full
+# do the best to suggest possible values
+yq -r "$hieraloc | $enterheatfuncs | $isheatfunc" \
+  $PUPPET $THT | awk -F '": ' '/::/ {if ($1) print}' | \
+  sed -r 's/\"//g;s/::/_/g;s/^\s+(.*)/\1/;s/,$//g;/ \{$/d' > /tmp/${SVC}_config_special
+
+# just a bulk raw view into related $SVC hiera keys
+yq -r "$hieraloc" $THT $PUPPET | grep  :: |\
   awk -F '": ' '/::/ {if ($1) print $1}' | \
   sed -r 's/\"//g;s/::/_/g;s/^\s+(.*)/\1/' | sort -u > /tmp/${SVC}_config
-yq -r "$namespace" $PUPPET | awk  '/::.*[^\{]$/ {print}' >> /tmp/${SVC}_config_defaults
-yq -r "$namespace" $PUPPET | \
-  awk -F '": ' '/::/ {if ($1) print $1}' | \
-  sed -r 's/\"//g;s/::/_/g;s/^\s+(.*)/\1/' | sort -u >> /tmp/${SVC}_config
 
 # top scope vars dedined in svc role defaults
 yq -r '. | keys[]' $VARS | sort -h > /tmp/${SVC}_sr
+
+
 # new ansible svc config data that doesn't map into t-h-t hiera data
 yq -r ".tripleo_${SVC}_config" $VARS > /tmp/${SVC}_src
 
 touch /tmp/${SVC}_fnames
 # produces lines to match tht params with vars, with columns:
-# original snake_case, prefix, short name (uniq key), full var name
-while read p; do
+# tht param as is, tht param snake_case, prefix, short name (uniq key), full role var name
+while read -r o p; do
   p2=$(sed -r "s/_$SVC//g" <<< $p)
   tht=$(sed -r "s/^($MATCH)|_$SVC|${SVC}_//g" <<< $p2)
   pref=$(sed -r "s/^($MATCH)\S+/\1/" <<< $p2)
   if [ "$pref" = "$tht" ] || grep -qE $MATCH <<< $pref ; then
-    pref=tripleo_${SVC}_
+    pref=${PREFIX}
   else
-    pref="tripleo_${SVC}_${pref}"
+    pref=${PREFIX}${pref}
   fi
   fname=$(sed -r "s/($SVC)_\1/\1/g;s/(tripleo_${SVC}_)($MATCH)(.*)/\1\3/g" <<< $pref$tht)
   if [ $(grep -q " $fname " /tmp/${SVC}_fnames | wc -l) -gt 1 ]; then
-    echo "ERROR: $fname cannot be defined more than once. Stopping."
+    echo "ERROR $fname: cannot be defined more than once. Stopping."
     exit 1
   fi
-  echo $p $pref $tht $fname
+  echo $o $p $pref $tht $fname
 done < /tmp/${SVC}_snake > /tmp/${SVC}_fnames
 
-# produces lines with a prefix, short and full names to match puppet hiera data
-# with vars
+# produces lines with
+# strict name, prefix, short name (uniq key) and full role var name,
+# to match puppet hiera data with role vars
 while read p; do
-  p=$(sed -r "s/_$SVC//g" <<< $p)
-  tht=$(sed -r "s/^($MATCH|tripleo_profile_base_)|_$SVC|${SVC}_//g" <<< $p)
-  pref=$(sed -r "s/^($MATCH|tripleo_profile_base_)\S+/\1/" <<< $p)
+  p2=$(sed -r "s/_$SVC//g" <<< $p)
+  tht=$(sed -r "s/^($MATCH|tripleo_profile_base_)|_$SVC|${SVC}_//g" <<< $p2)
+  pref=$(sed -r "s/^($MATCH|tripleo_profile_base_)\S+/\1/" <<< $p2)
   if [ "$pref" = "$tht" ] || [ "$pref" = "tripleo_profile_base_" ] || grep -qE $MATCH <<< $pref ; then
-    pref=tripleo_${SVC}_
+    pref=${PREFIX}
   else
-    pref="tripleo_${SVC}_${pref}"
+    pref=${PREFIX}${pref}
   fi
   # dedup repeated service names in the vars names
   # relax t-h-t following naming rules for ansible vars to keep it shorter
   fname=$(sed -r "s/($SVC)_\1/\1/g;s/(tripleo_${SVC}_)($MATCH)(.*)/\1\3/g" <<< $pref$tht)
-  echo $pref $tht $fname
+  echo $p $pref $tht $fname
 done < /tmp/${SVC}_config > /tmp/${SVC}_cnames
 
-while IFS='  ' read -r o p n fn; do
+# tht param as is, tht param snake_case, prefix, short name (uniq key), full role var name
+while IFS='  ' read -r o p pr n fn; do
   if [ $(grep -q " $n " /tmp/${SVC}_fnames | wc -l) -gt 1 ]; then
-    echo "ERROR: $n cannot be defined more than once. Stopping."
+    echo "ERROR $n: cannot be defined more than once. Stopping."
     exit 1
   fi
   # To find missing vars by unmatching t-h-t params
@@ -118,23 +151,66 @@ while IFS='  ' read -r o p n fn; do
     continue
   fi
   if ! grep -q $n /tmp/${SVC}_sr && ! grep -q $n $VARS ; then
-    echo "Var for $n t-h-t param looks missing, use name $fn ?"
+    echo "ERROR $fn: mapping to t-h-t param looks missing"
     continue
   fi
   # prepare string to wire-in it into ansible group vars in t-h-t
-  sed -r -i "s/^$o:/$fn:/g" /tmp/${SVC}_group_vars_wire_in
+  sed -r -i "s/^$p:/$fn:/g" /tmp/${SVC}_group_vars_wire_in
 done < /tmp/${SVC}_fnames
 
-# FIXME: maybe tht keys and hiera data needs another ignore lists
-while IFS='  ' read -r p n fn; do
-  # To find missing vars by unmatching hiera data keys
-  # (also look it up in new ansible config data)
-  grep -q $n <<< $IGNORE && continue
+# To find missing vars by unmatching hiera data keys,
+# also look it up in new ansible config data var names;
+# exclude all duplicates among role vars mapped to tht params and hiera keys
+#
+# strict name, prefix, short name (uniq key), full role var name
+while IFS='  ' read -r s p n fn; do
+  # find hiera mapped role vars duplicated by role vars mapped to tht params,
+  standard_name_match=$(sed -r "s/_|::/\(_\|::\)/g" <<< $n)
+  lookup=$(grep -E "_?${standard_name_match}\b" /tmp/${SVC}_config_special)
+  # does looked up hiera data match a direct get_param mapping?
+  tht_param=$(awk -F': ' '{print $2}'<<< $lookup)
+  if [ "$tht_param" ] && grep -qE "^$tht_param " /tmp/${SVC}_fnames; then
+    role_var=$(awk "/^$tht_param / {print \$NF}" /tmp/${SVC}_fnames)
+    if [ "$role_var" != "$fn" ] && ! grep -qE "$standard_name_match" <<< $role_var ; then
+      if grep -qE "\b${role_var}\b" /tmp/${SVC}_sr; then
+        echo "WARNING $fn: removed dup of $role_var: hiera assignment of $tht_param"
+        sed -ri "/^$fn:/d" "$VARS"
+        continue
+      fi
+      echo "WARNING $fn: renamed to $role_var: $tht_param wins over hiera mapping"
+      sed -ri "s/^$fn:/$role_var:/g" "$VARS"
+      continue
+    fi
+  fi
+  # find duplicating vars mapped to tht params and puppet hiera data
+  dup=$(jq -r "select(.key|test(\"${standard_name_match}\")) | .value.get_param" /tmp/${SVC}_config_substitutions)
+  keyname=$(jq -r "select(.key|test(\"${standard_name_match}\")) | .key" /tmp/${SVC}_config_substitutions)
+  if [ "$dup" ] && ! grep -qE "$standard_name_match" <<< $fn ; then 
+    role_var=$(awk "/^$dup / {print \$NF}" /tmp/${SVC}_fnames)
+    if grep -qE "\b${fn}\b" /tmp/${SVC}_sr; then
+      echo "WARNING $fn: removed dup of $role_var: hiera $keyname: {get_param: $dup}"
+      sed -ri "/^$fn:/d" "$VARS"
+      continue
+    fi
+    echo "WARNING $fn: renamed to $role_var: $dup wins over hiera mapping"
+    sed -ri "s/^$fn:/$role_var:/g" "$VARS"
+    continue
+  fi
   if ! grep -q $n /tmp/${SVC}_sr && ! grep -q $n /tmp/${SVC}_src && ! grep -q $n $VARS ; then
-    echo "Var for $n hiera key looks missing, use:"
-    default=$(grep -E "$(sed -r "s/_/\(_\|::\)/g" <<< $n)" /tmp/${SVC}_config_defaults | \
-      awk -F': ' '!/\{/ {if ($2) print $2}' | tr -s ',' '\n')
-    echo "${fn}: $default"
+    # when relaxed naming rule didn't match the original strict name,
+    # fallback to orignal name in *_config as well to pick a tht default
+    strict_name_match=$(sed -r "s/_|::/\(_\|::\)/g" <<< $s)
+    default=$(jq -r  "select(.key|test(\"${standard_name_match}|${strict_name_match}\")) | .value" /tmp/${SVC}_config_defaults /tmp/${SVC}_config_special_full)
+    if [ "${default}${lookup}" ]; then
+      echo "ERROR $fn: mapping to hiera key looks missing: matching t-h-t value: ${lookup:-$default}"
+      continue
+    fi
+    echo "ERROR $fn: mapping to hiera key looks missing: see t-h-t definition: $default"
+    # looking for a better way to show enclosing object (heat funcs stack)
+    snippet=$(grep -E -C10 "_?$standard_name_match\b" /tmp/${SVC}_config_special_full)
+    [ "$snippet" ] || continue
+    echo " --------- code snipped ------------"
+    echo "$snippet"
   fi
 done < /tmp/${SVC}_cnames
 
@@ -145,14 +221,15 @@ while read p; do
   # but leaving foo_real as a valid match for a foo
   m=$(sed -r "s/^tripleo_($MATCH|${SVC}_|_${SVC}$)//g" <<< $p)
   m=$(sed -r "s/(\S+)_real/\1/g" <<< $m)
-  grep -q -E $m <<< $IGNORE && continue
+  grep -q $m <<< $IGNORE && continue
   grep -q -E "${ignored%|*}" <<< $m && continue
   if ! grep -q $m /tmp/${SVC}_fnames && ! grep -q $m /tmp/${SVC}_cnames && ! grep -q $m /tmp/${SVC}_src; then
-    echo "Var for $m looks redundant and has been deleted"
+    echo "WARNING $m: removed as redundant: no t-h-t param, nor hiera mapping found"
     sed -ri "/^$p:/d" "$VARS"
   fi
+  # also remove redundant var that map hiera data directly substituted by other vars
 done < /tmp/${SVC}_sr
 
 echo
-echo "Group vars to wire-in for t-h-t to call the role:"
+echo "INFO: Group vars to wire-in for t-h-t to call the role:"
 cat /tmp/${SVC}_group_vars_wire_in
